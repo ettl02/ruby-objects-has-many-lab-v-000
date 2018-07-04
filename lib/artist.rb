fd3a6bc4350a7adb3c0c32b97c37101567c352e6
Emily Wessel <ettl02@gmail.com>
2018-07-04 18:11:44 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,9 +5,15 @@ def initialize (name)
   @name = name
     @song = []
 end
-end
 
 def add_song (song)
   @songs << song
+  song.artist = self 
+end
+
+def add_song_by_name (name)
+  song = Song.new (name)
+  @songs << song 
+  song.artist = self 
 end 
 end
