3acb3227086a095e3316b423815c0f6bdc843341
Emily Wessel <ettl02@gmail.com>
2018-07-04 18:12:26 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -8,12 +8,16 @@ end
 
 def add_song (song)
   @songs << song
-  song.artist = self 
+  song.artist = self
 end
 
 def add_song_by_name (name)
   song = Song.new (name)
-  @songs << song 
-  song.artist = self 
+  @songs << song
+  song.artist = self
+end
+
+def song_count 
+  @songs.count 
 end 
 end
