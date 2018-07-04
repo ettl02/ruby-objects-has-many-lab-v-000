5a0d558554e58d487d80d41737d289179215bfd8
Emily Wessel <ettl02@gmail.com>
2018-07-04 18:52:17 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,11 +1,21 @@
 class Author
   attr_accessor :name
+  
+@@all = []
 
   def initialize (name)
     @name = name
+    @@all << self 
   end
-end
 
+  def self.all
+    @@all
+  end
+
+  def add_post (post)
+    post.author = self 
+  end 
+  
   def add_post_by_title (title)
     post = Post.new (title, self)
   end
@@ -13,4 +23,8 @@ end
   def posts
     Post.all.select { |post| post.author == self }
   end
+
+  def self.post_count 
+    Post.all.count
+  end 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -15,9 +15,8 @@ class Song
   def artist_name
     if self.artist
       self.artist.name
-    else 
-      nil 
-    end 
+    else
+      nil
+    end
   end
-end 
-
+end
